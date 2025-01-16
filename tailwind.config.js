d513b6c082a2896dcd257314d761c099a0392e03
Hardi99 <harditabuna@gmail.com>
2025-01-16 21:49:47 +0100

    Insertion image + fin de séance 01

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1d4ed8",
+          "secondary": "#1e40af",
+          "accent": "#9ca3af",
+          "neutral": "#ff00ff",
+          "base-100": "#ffffff",
+          "info": "#38bdf8",
+          "success": "#2ecc71",
+          "warning": "#eab308",
+          "error": "#dc2626",
+          },
+        },
+      ],
+    },
   plugins: [require('daisyui'),
   ],
 }
