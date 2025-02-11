6065f022a123e33a6f90d4f8ffb45607d6b8cd98
Guelor Abel ANGO <gueloraby@gmail.com>
2025-02-11 19:12:18 +0100

    Ajout du routerActive dans la navigation

--- src/app/shared/nav-menu/nav-menu.component.ts
+++ src/app/shared/nav-menu/nav-menu.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { BoxContentComponent } from '../box-content/box-content.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '@app/core/services/cart.service';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [BoxContentComponent, RouterLink],
+  imports: [BoxContentComponent, RouterLink, RouterLinkActive],
   templateUrl: './nav-menu.component.html',
   styles: ``
 })
